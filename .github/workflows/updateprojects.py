adb92da0beeed4c1e35cba1f9c9b51cb6059f6a3
John Mertic <jmertic@linuxfoundation.org>
2021-07-16 15:58:08 -0400

    pull data from landscape
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

---
+++ .github/workflows/updateprojects.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3                                                                                                  
+#                                                                                                                       
+# Copyright this project and it's contributors                                                                          
+# SPDX-License-Identifier: Apache-2.0                                                                                   
+#                                                                                                                       
+# encoding=utf8
+
+import csv
+import urllib.request
+import json
+import os
+
+projectsCsvFile = os.path.dirname(os.path.realpath(__file__))+'/../../_data/projects.csv'
+
+landscapeBaseURL = 'https://landscape.openmainframeproject.org'
+landscapeHostedProjects = landscapeBaseURL+'/data/exports/projects-hosted.json'
+landscapeSingleItem = landscapeBaseURL+'/data/items/{}.json'
+
+# map landscape ID to slug
+keyMapping = {
+    'open-mainframe-education': 'mainframe-open-education' 
+}
+
+csvRows = []
+
+with urllib.request.urlopen(landscapeHostedProjects) as hostedProjectsResponse:
+    for projectStage in json.load(hostedProjectsResponse):
+        for project in projectStage['items']:
+            with urllib.request.urlopen(landscapeSingleItem.format(project['id'])) as singleItemResponse:
+                projectData = json.load(singleItemResponse)
+                print("Processing {}...".format(projectData['name']))
+                csvRows.append({
+                        'Name': projectData['name'],
+                        'Level': projectData['project'],
+                        'Logo URL': project['logo'],
+                        'Slug': projectData['id'],
+                        'Website': projectData['homepage_url'],
+                        'Leads': projectData['extra']['leads'],
+                        'Meeting Cadence': projectData['extra']['meeting_cadence'],
+                        'LFX Insights URL': projectData['extra']['lfx_insights_url'] if 'lfx_insights_url' in projectData['extra'] else None,
+                        'Accepted Date': projectData['extra']['date_accepted'],
+                        'Last Review Date': projectData['extra']['last_review_date'],
+                        'Next Review Date': projectData['extra']['next_review_date'],
+                        'Slack': projectData['extra']['slack_channel'],
+                        'Mailing List': projectData['extra']['mailing_list_url'] if 'mailing_list_url' in projectData['extra'] else None,
+                        'User Mailing List': projectData['extra']['user_mailing_list_url'] if 'user_mailing_list_url' in projectData['extra'] else None,
+                        'Dev Mailing List': projectData['extra']['dev_mailing_list_url'] if 'dev_mailing_list_url' in projectData['extra'] else None,
+                        'Primary Github Repo': projectData['project_org'] if 'project_org' in projectData else None,
+                        'Github Org': projectData['repo_url']
+                        })
+
+with open(projectsCsvFile, 'w') as projectsCsvFileObject:
+    writer = csv.DictWriter(projectsCsvFileObject, fieldnames = csvRows[0].keys())
+    writer.writeheader() 
+    writer.writerows(csvRows) 
--- engagement/collaboration_lists.md
+++ engagement/collaboration_lists.md
@@ -13,16 +13,28 @@ All Slack channels referenced below are part of the [Open Mainframe Project Slac
 <tbody>
 {%- for project in site.data.projects -%}
     <tr>
-        <td>{{ project["Name"] }}</td>
+        <td><img src="{{ project["Logo URL"] }}" /></td>
         <td>
             {%- if project["Website"] -%}
             Website: <a href="{{ project["Website"] }}">{{ project["Website"] }}</a><br />
+            {%- endif -%}
+            {%- if project["Dev Mailing List"] -%}
+            Dev Mailing List: <a href="{{ project["Dev Mailing List"] }}">{{ project["Dev Mailing List"] }}</a><br />
+            {%- endif -%}
+            {%- if project["User Mailing List"] -%}
+            User Mailing List: <a href="{{ project["User Mailing List"] }}">{{ project["User Mailing List"] }}</a><br />
             {%- endif -%}
             {%- if project["Mailing List"] -%}
             Mailing List: <a href="{{ project["Mailing List"] }}">{{ project["Mailing List"] }}</a><br />
             {%- endif -%}
             {%- if project["Slack"] -%}
             Slack: <a href="https://slack.openmainframeproject.org">{{ project["Slack"] }}</a><br />
+            {%- endif -%}
+            {%- if project["Leads"] -%}
+            Leads: {{ project["Leads"] }}<br />
+            {%- endif -%}
+            {%- if project["Meeting Cadence"] -%}
+            Meeting Cadence: {{ project["Meeting Cadence"] }}<br />
             {%- endif -%}
         </td>
     </tr>    
